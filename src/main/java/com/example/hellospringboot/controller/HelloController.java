b328f7c3825c07478090df64d32c75d5c397afde
ChenH <chenhao6319@163.com>
2023-11-07 23:30:43 +0800

    [SpringBoot] 获取GET和POST请求参数

--- src/main/java/com/example/hellospringboot/controller/HelloController.java
+++ src/main/java/com/example/hellospringboot/controller/HelloController.java
@@ -1,12 +1,23 @@
 package com.example.hellospringboot.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
-    @GetMapping("/hello")
-    public String hello() {
-        return "hello world";
+    @RequestMapping(value = "/hello1", method = RequestMethod.GET)
+    public String getHello1(String name, String age) {
+        return "name:" + name + "age" + age;
     }
+
+    // 使用@RequestParam将前端传递来的name映射为getHello2的name1使用
+    @RequestMapping(value = "/hello2", method = RequestMethod.GET)
+    public String getHello2(@RequestParam(value = "name", required = false) String name1, @RequestParam(value = "age", required = false) String age1) {
+        return "name:" + name1 + "age" + age1;
+    }
+    // Content-Type: application/x-www-form-urlencoded
+    @RequestMapping(value = "/hello3", method = RequestMethod.POST)
+    public String postHello1(String name, String age) {
+        return "POST请求" + "name:" + name + "age" + age;
+    }
+    // TODO 使用@RequestBody从Content-Type: application/json获取参数
 }
